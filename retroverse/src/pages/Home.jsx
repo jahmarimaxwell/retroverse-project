cf921b40a1baea11a2c29b8cd301e1bbf04941fc
Mari300 <jahfly9@gmail.com>
2025-11-10 02:47:26 -0500

    added retroverse picture and sparkle effect

--- retroverse/src/pages/Home.jsx
+++ retroverse/src/pages/Home.jsx
@@ -1,18 +1,15 @@
 const Home = () => {
     return (
     <>
-    <div className="content-container">
-      <h1 className="title">About</h1>
-        <div className="inner-content-contiainer"> {/* style={{ backgroundImage: `url()`}}> */}
-            <p className="description"> 
-                Dive into the vast Marvel Universe 
-                with our interactive platform that brings your favorite 
-                heroes and villains to life. Powered by the Marvel API, 
-                our site offers a dynamic, up-to-date display of Marvel characters, 
-                featuring detailed profiles, iconic images, and fascinating backstories. 
-                Whether you're a die-hard Marvel fan or new to the universe, 
-                explore and discover characters from across the Marvel multiverse, all in one place. 
-            </p>
+    <div className="overall-container">
+        <div className="logo-container">
+            <div class="sparkle"></div> 
+            <img className="site-title-logo" src="/pictures/retroverse-title.png" alt="Site Logo"/>
+        </div>
+        <div className="content-container">
+            <div className="inner-content-container">
+            
+            </div>
         </div>
     </div>
     </>
