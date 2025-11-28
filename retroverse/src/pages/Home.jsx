ffc1369fdef70a4889ff96e827bfcc8ee341532f
Mari300 <jahfly9@gmail.com>
2025-11-27 21:20:26 -0500

    establishing a format for retoverse

--- retroverse/src/pages/Home.jsx
+++ retroverse/src/pages/Home.jsx
@@ -7,10 +7,32 @@ const Home = () => {
             <img className="site-title-logo" src="/pictures/retroverse-title.png" alt="Site Logo"/>
         </div>
         <div className="content-container">
-            <div className="inner-content-container">
-            
+            <div className="icon-container">
+                <img src="/pictures/playstation-psn-icon-template.png"/>
             </div>
+                <div className="description-container">
+                    <h1 className="title"> Example Title </h1>
+                    <div className="description">
+                        An example description and just a bunch of random text
+                    </div>
+                </div>
         </div>
+
+        <hr></hr>
+
+        <div className="content-container">
+            <div className="icon-container">
+                <img src="/pictures/ps2-icon-template.png"/>
+            </div>
+                <div className="description-container">
+                    <h1 className="title"> Example Title </h1>
+                    <div className="description">
+                        An example description and just a bunch of random text
+                    </div>
+                </div>
+        </div>
+
+        <hr></hr>
     </div>
     </>
     )
